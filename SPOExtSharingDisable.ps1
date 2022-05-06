6245abe28140c1ade01ce9648fb0cb3e5e557502
Mehmet PARLAKYİĞİT [MVP] <53214224+mparlakyigit@users.noreply.github.com>
2022-05-06 18:48:59 +0300

    Add files via upload

---
+++ SPOExtSharingDisable.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.HAKKIMDA
+	Mehmet PATLAKYİĞİT | Office Apps & Services MVP
+	https://www.parlakyigit.net/
+	Twitter:@mparlakyigit 
+#>
+
+<#
+.NOTE
+Bu PowerShell 
+#>
+
+Write-Host "Mehmet PATLAKYİĞİT | Office Apps & Services MVP" -ForegroundColor blue
+Write-Host "https://www.parlakyigit.net/" -ForegroundColor blue
+Write-Host "Twitter:@mparlakyigit" -ForegroundColor blue
+
+
+$Org_Name = "mparlakyigit"
+#https://sizin_organizasyonunuz-admin.sharepoint.com/
+
+
+Write-Output "SharePoint Online Servisine Bağlanılıyor."
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+Connect-SPOService -Url "https://$Org_Name-admin.sharepoint.com"
+
+Set-SPOTenant -SharingCapability Disabled
